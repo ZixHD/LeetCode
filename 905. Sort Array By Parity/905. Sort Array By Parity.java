4551ae87e8d3af91eb8f77aa0bf17f369670bc67
ZixHD <113049970+ZixHD@users.noreply.github.com>
2023-08-01 20:04:39 +0200

    Create 905. Sort Array By Parity.java

---
+++ 905. Sort Array By Parity/905. Sort Array By Parity.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+       int index=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                int temp=nums[index];
+                nums[index]=nums[i];
+                nums[i]=temp;
+                index++;
+            }
+        }
+        return nums;
+    }
+}
