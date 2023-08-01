a87ec2e339b018345f20f4d6062cd6fbfaf55fc6
ZixHD <113049970+ZixHD@users.noreply.github.com>
2023-08-01 19:59:21 +0200

    Create 561. Array Partition.java

---
+++ 561. Array Partition/561. Array Partition.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int suma=0;
+        for(int i=0;i<nums.length-1;i+=2){
+            suma+=Math.min(nums[i],nums[i+1]);
+        }
+
+        return suma;
+    }
+}
