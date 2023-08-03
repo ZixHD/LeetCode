28873b9abb83033c345fa1571ef2299137185697
ZixHD <113049970+ZixHD@users.noreply.github.com>
2023-08-03 16:30:21 +0200

    Create 739. Daily Temperatures.java

---
+++ 739. Daily Temperatures/739. Daily Temperatures.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[temperatures.length];
+        for(int i=0;i<temperatures.length;i++){
+            while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()]){
+                int index = stack.pop();
+                ans[index] = i - index;
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+}
