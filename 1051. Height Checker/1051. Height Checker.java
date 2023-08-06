a53fba5ba899b31765567316753ef722c6e54176
ZixHD <113049970+ZixHD@users.noreply.github.com>
2023-08-06 13:32:54 +0200

    Update 1051. Height Checker.java

--- 1051. Height Checker/1051. Height Checker.java
+++ 1051. Height Checker/1051. Height Checker.java
@@ -1,1 +1,14 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        
+        int[] ans = heights.clone();
+        Arrays.sort(ans);
+        int count = 0;
+        for(int i = 0; i < heights.length; i++){
+            if(heights[i]!=ans[i])
+                count++;
+        }
 
+        return count;
+    }
+}
