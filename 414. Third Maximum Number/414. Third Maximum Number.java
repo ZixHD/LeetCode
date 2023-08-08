24c68fcc263b65fb32b64042ad277ded0284c084
ZixHD <113049970+ZixHD@users.noreply.github.com>
2023-08-08 13:14:28 +0200

    Create 414. Third Maximum Number.java

---
+++ 414. Third Maximum Number/414. Third Maximum Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Set<Integer> set = new HashSet<>();
+        for(int n : nums){
+            if(set.add(n)){
+                pq.offer(n);
+                if(pq.size()>3) pq.poll();
+            }
+        }
+        if(pq.size() == 2) pq.poll();
+        return pq.peek();
+    }
+}
