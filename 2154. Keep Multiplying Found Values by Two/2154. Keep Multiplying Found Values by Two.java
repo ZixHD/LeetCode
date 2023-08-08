c73e8a32d400cc570d502d22d0ea43cef23bcc27
ZixHD <113049970+ZixHD@users.noreply.github.com>
2023-08-08 13:52:46 +0200

    Create 2154. Keep Multiplying Found Values by Two.java

---
+++ 2154. Keep Multiplying Found Values by Two/2154. Keep Multiplying Found Values by Two.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        
+        Set<Integer> set = new HashSet<>();
+        for(int i : nums){
+          if(i >= original){
+              set.add(i);
+          }
+        }
+        while(true){
+            if(set.contains(original)){
+                original*=2;
+            }else break;
+        }
+        return original;
+    }
+}
