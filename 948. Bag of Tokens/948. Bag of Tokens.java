605a97569df972ae6cb0dd6444a95ad558425886
ZixHD <113049970+ZixHD@users.noreply.github.com>
2023-08-16 13:33:18 +0200

    Update 948. Bag of Tokens.java

--- 948. Bag of Tokens/948. Bag of Tokens.java
+++ 948. Bag of Tokens/948. Bag of Tokens.java
@@ -1,1 +1,23 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        
+        Arrays.sort(tokens);
+        int score = 0;
+        int left = 0, right = tokens.length - 1;
+        while(left <= right){
 
+            if(power >= tokens[left]){
+                score++;
+                power-= tokens[left];
+                left++;
+            }
+            else if(score>=1 && left != right){
+                power+=tokens[right];
+                right--;
+                score--;
+            }else break;
+        }
+
+        return score;
+    }
+}
