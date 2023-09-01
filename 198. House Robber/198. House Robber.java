3cc50f694d5ff5470b41e9a102aebf66fe558d51
ZixHD <113049970+ZixHD@users.noreply.github.com>
2023-09-01 21:39:20 +0200

    Create 198. House Robber.java

---
+++ 198. House Robber/198. House Robber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+    
+    if (nums.length == 0) return 0;
+    int a = 0;
+    int b = 0;
+    for(int i = 0; i < nums.length; i++){
+        
+        if(i % 2 == 0){
+            a = Math.max(a + nums[i], b);
+        }
+        else b = Math.max(a, b + nums[i]);
+    }
+
+    return Math.max(a, b);
+
+    }
+}
